ab727a999f40133983bbd89f0636fc9cbf7f6fee
lateef-Blessing <lateeftijani66@gmail.com>
2024-11-12 01:57:40 +0100

    mail

--- lib/mail-template.ts
+++ lib/mail-template.ts
@@ -21,7 +21,7 @@ export function renderEventDeletionEmailTemplate(event: Event) {
       <h3 style="font-size: 18px; color: #0077B5;"><strong>Event Details: </strong>${
         event.description
       }</h3>
-      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString()}</h3>
+      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString}</h3>
       <p>We regret to inform you that the event with the above details has been cancelled by the organizer and your deposit has been refunded.</p>
       <p>If you have any questions or need assistance, our support team is always here to help.</p>
       <p>Best regards,<br>The Atendeo Team</p>
@@ -52,7 +52,7 @@ export function renderEventAttendanceDeletionEmailTemplate(event: Event) {
       <h3 style="font-size: 18px; color: #0077B5;"><strong>Event Details: </strong>${
         event.description
       }</h3>
-      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString()}</h3>
+      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString}</h3>
       <p>Your attendance to the upcoming event has been cencelled and half of your deposit has been refunded.</p>
       <p>If you have any questions or need assistance, our support team is always here to help.</p>
       <p>Best regards,<br>The Atendeo Team</p>
@@ -83,7 +83,7 @@ export function renderEventNotificationEmailTemplate(event: Event) {
       <h3 style="font-size: 18px; color: #0077B5;"><strong>Event Details: </strong>${
         event.description
       }</h3>
-      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString()}</h3>
+      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString}</h3>
       <p>This email is sent to you to remind you of the upcoming event with the details above.</p>
       <p>If you have any questions or need assistance, our support team is always here to help.</p>
       <p>Best regards,<br>The Atendeo Team</p>
@@ -114,7 +114,7 @@ export function renderEventPresentMemberEmailTemplate(event: Event) {
       <h3 style="font-size: 18px; color: #0077B5;"><strong>Event Details: </strong>${
         event.description
       }</h3>
-      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString()}</h3>
+      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString}</h3>
       <p>This is to notify you that this event has been concluded and your account has been refunded with your deposit and flaked members bonus successfully added to your balance and available for withdrawal.</p>
       <p>If you have any questions or need assistance, our support team is always here to help.</p>
       <p>Best regards,<br>The Atendeo Team</p>
@@ -145,7 +145,7 @@ export function renderEventAbsentMemberEmailTemplate(event: Event) {
       <h3 style="font-size: 18px; color: #0077B5;"><strong>Event Details: </strong>${
         event.description
       }</h3>
-      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString()}</h3>
+      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString}</h3>
       <p>This is to notify you that you missed this event and your deposit has been splitted between the present members.</p>
       <p>If you have any questions or need assistance, our support team is always here to help.</p>
       <p>Best regards,<br>The Atendeo Team</p>
@@ -176,7 +176,7 @@ export function renderEventMessageEmailTemplate(event: any, message: string) {
       <h3 style="font-size: 18px; color: #0077B5;"><strong>Event Details: </strong>${
         event.description
       }</h3>
-      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString()}</h3>
+      <h3 style="font-size: 18px; color: #0077B5;"><strong>Scheduled For: </strong>${event.eventDate.toDateString}</h3>
       <p>The message below is sent to you by the organiser of this event</p>
       <p>${message}</p>
       <p>If you have any questions or need assistance, our support team is always here to help.</p>
