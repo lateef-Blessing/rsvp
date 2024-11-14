16ea3d66d1a62a231e8427280978600e236bfedf
lateef-Blessing <lateeftijani66@gmail.com>
2024-11-14 17:00:51 +0100

    reverted back

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,7 +1,14 @@
 import { Event } from "@prisma/client";
 import nodemailer from "nodemailer";
-import { renderEventAbsentMemberEmailTemplate, renderEventAttendanceDeletionEmailTemplate, renderEventDeletionEmailTemplate, renderEventMessageEmailTemplate, renderEventNotificationEmailTemplate, renderEventPresentMemberEmailTemplate } from "@/lib/mail-template";
-import { formatDateTime } from "@/lib/utils";
+
+import {
+  renderEventAbsentMemberEmailTemplate,
+  renderEventAttendanceDeletionEmailTemplate,
+  renderEventDeletionEmailTemplate,
+  renderEventMessageEmailTemplate,
+  renderEventNotificationEmailTemplate,
+  renderEventPresentMemberEmailTemplate,
+} from "@/lib/mail-template";
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
@@ -15,7 +22,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// Auth Mails 
+// Auth Mails
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   try {
     const mail = await transporter.sendMail({
@@ -60,13 +67,16 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 // Event Mails
-export const sendEventNotificationEmail = async (email: string, event: Event) => {
+export const sendEventNotificationEmail = async (
+  email: string,
+  event: Event
+) => {
   try {
     const mail = await transporter.sendMail({
       from: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
       to: email,
       subject: `[ATENDEO] Event Notification`,
-      html: renderEventNotificationEmailTemplate(event)
+      html: renderEventNotificationEmailTemplate(event),
     });
   } catch (error) {
     console.log(error);
@@ -79,20 +89,23 @@ export const sendEventDeletionEmail = async (email: string, event: Event) => {
       from: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
       to: email,
       subject: "[ATENDEO] Event Cancellation Notification",
-      html: renderEventDeletionEmailTemplate(event)
+      html: renderEventDeletionEmailTemplate(event),
     });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const sendEventAttendanceDeletionEmail = async (email: string, event: Event) => {
+export const sendEventAttendanceDeletionEmail = async (
+  email: string,
+  event: Event
+) => {
   try {
     await transporter.sendMail({
       from: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
       to: email,
       subject: "[ATENDEO] Event Attendance Cancellation Notification",
-      html: renderEventAttendanceDeletionEmailTemplate(event)
+      html: renderEventAttendanceDeletionEmailTemplate(event),
     });
   } catch (error) {
     console.log(error);
@@ -105,7 +118,7 @@ export const sendPresentMembersEmail = async (email: string, event: Event) => {
       from: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
       to: email,
       subject: `You were present at this event`,
-      html: renderEventPresentMemberEmailTemplate(event)
+      html: renderEventPresentMemberEmailTemplate(event),
     });
   } catch (error) {
     console.log(error);
@@ -118,39 +131,24 @@ export const sendAbsentMembersEmail = async (email: string, event: Event) => {
       from: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
       to: email,
       subject: `You flaked this event`,
-      html: renderEventAbsentMemberEmailTemplate(event)
+      html: renderEventAbsentMemberEmailTemplate(event),
     });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const sendEventMessageEmail = async (email: string, message: string, event: Event) => {
+export const sendEventMessageEmail = async (
+  email: string,
+  message: string,
+  event: Event
+) => {
   try {
     const mail = await transporter.sendMail({
       from: process.env.NEXT_PUBLIC_PERSONAL_EMAIL,
       to: email,
       subject: `Message from Event Organiser`,
-      html: `
-    <div style="background-color: black; padding: 20px; text-align: center; border-radius: 10px 10px 0 0;">
-      <img src=${`${process.env.NEXT_PUBLIC_APP_URL}/bl-nobg.png`} alt="Atendeo Logo" style="width: 300px; height: 300px;">
-    </div>
-    <div style="background-color: #ffffff; padding: 10px; border-radius: 0 0 10px 10px; box-shadow: 0 4px 10px rgba(0,0,0,0.1);">
-      <h3><strong style="font-size: 18px; color: #3b82f6;">Event Title: </strong>${
-        event.title
-      }</h3>
-      <h3><strong style="font-size: 18px; color: #3b82f6;">Event Details: </strong>${
-        event.description
-      }</h3>
-      <h3><strong style="font-size: 18px; color: #3b82f6;">Scheduled For: </strong>${
-        formatDateTime(event.eventDate).dateTime
-      }</h3>
-      <p>The message below is sent to you by the organiser of this event</p>
-      <p>${message}</p>
-      <p style="color: gray; margin-top: 30px;">If you have any questions or need assistance, our support team is always here to help.</p>
-      <p style="color: gray;">Best regards,<br>The Atendeo Team</p>
-    </div>
-  `
+      html: renderEventMessageEmailTemplate(event, message),
     });
   } catch (error) {
     console.log(error);
